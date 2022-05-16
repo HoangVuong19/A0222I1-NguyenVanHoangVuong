24693e2028ba58bd5c0bd5f4fe4a846a084b86df
HoangVuong19 <vuonghoang0208@gmail.com>
2022-05-16 19:38:25 +0700

    nop bai ngay 16-05

---
+++ module2/src/_01_introduction_java/thuc_hanh/bai_hien_thi_thoi_gian/SystemTime.java
@@ -0,0 +1,11 @@
+package _01_introduction_java.thuc_hanh.bai_hien_thi_thoi_gian;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
