b9f2ab06efeb806fa8b607d8dc2c1d8d27e5080f
HoangVuong19 <vuonghoang0208@gmail.com>
2022-05-18 19:27:25 +0700

    nop bai ngay 18-05

---
+++ module2/src/_02_loop_java/thuc_hanh/bai_tinh_tien_lai_cho_vay/Main.java
@@ -0,0 +1,28 @@
+package _02_loop_java.thuc_hanh.bai_tinh_tien_lai_cho_vay;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double money = 1.0;
+        int month = 1;
+        double interestRate = 1.0;
+
+        System.out.println("Enter investment amount: ");
+        money = scanner.nextDouble();
+
+        System.out.println("Enter number of months: ");
+        month = scanner.nextInt();
+
+        System.out.println("Enter annual interest rate in percentage: ");
+        interestRate = scanner.nextDouble();
+
+        double totalInterest = 0;
+        for (int i = 0; i < month; i++) {
+            totalInterest += money * (interestRate / 100) / 12 * month;
+        }
+
+        System.out.println("Total of interest: " + totalInterest);
+    }
+}
