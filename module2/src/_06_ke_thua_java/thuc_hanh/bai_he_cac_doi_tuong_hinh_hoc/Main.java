3eee2e57916d6496a61c2478151976b0245c8915
HoangVuong19 <vuonghoang0208@gmail.com>
2022-05-29 21:50:38 +0700

    nop bai ngay 29-05

---
+++ module2/src/_06_ke_thua_java/thuc_hanh/bai_he_cac_doi_tuong_hinh_hoc/Main.java
@@ -0,0 +1,41 @@
+package _06_ke_thua_java.thuc_hanh.bai_he_cac_doi_tuong_hinh_hoc;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+
+
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+
+
+        Square square = new Square();
+        System.out.println(square);
+
+        square = new Square(2.3);
+        System.out.println(square);
+
+        square = new Square(5.8, "yellow", true);
+        System.out.println(square);
+    }
+}
