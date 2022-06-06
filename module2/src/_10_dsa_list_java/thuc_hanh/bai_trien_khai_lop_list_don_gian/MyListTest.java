c2256cf5d3ef01e0c3032bd9810ccaf79efd38e9
HoangVuong19 <vuonghoang0208@gmail.com>
2022-06-06 19:38:33 +0700

    nop bai ngay 06-06

---
+++ module2/src/_10_dsa_list_java/thuc_hanh/bai_trien_khai_lop_list_don_gian/MyListTest.java
@@ -0,0 +1,19 @@
+package _10_dsa_list_java.thuc_hanh.bai_trien_khai_lop_list_don_gian;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println("element 4: " + listInteger.get(3));
+        System.out.println("element 1: " + listInteger.get(0));
+        System.out.println("element 2: " + listInteger.get(1));
+
+        listInteger.add(6);
+        System.out.println("element 6: " + listInteger.get(5));
+    }
+}
