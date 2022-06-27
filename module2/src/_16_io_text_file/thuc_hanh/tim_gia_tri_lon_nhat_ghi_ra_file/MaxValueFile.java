b48b986568c656ffa62e2f066e792e976e467148
HoangVuong19 <vuonghoang0208@gmail.com>
2022-06-27 18:28:38 +0700

    nop bai ngay 27-06

---
+++ module2/src/_16_io_text_file/thuc_hanh/tim_gia_tri_lon_nhat_ghi_ra_file/MaxValueFile.java
@@ -0,0 +1,27 @@
+package _16_io_text_file.thuc_hanh.tim_gia_tri_lon_nhat_ghi_ra_file;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MaxValueFile {
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine();
+
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile(path);
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile(path, maxValue);
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (Integer number : numbers) {
+            if (max < number) {
+                max = number;
+            }
+        }
+        return max;
+    }
+}
---
+++ module2/src/_16_io_text_file/thuc_hanh/tinh_tong_cac_so_trong_file_text/ReadFileExample.java
@@ -0,0 +1,35 @@
+package _16_io_text_file.thuc_hanh.tinh_tong_cac_so_trong_file_text;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine();
+        //path: "src\\_16_io_text_file\\thuc_hanh\\tinh_tong_cac_so_trong_file_text\\numbers.txt"
+        readFileText(path);
+    }
+
+    public static void readFileText(String filePath) {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader br = Files.newBufferedReader(file.toPath());
+            String line;
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+            System.out.println(sum);
+        } catch (Exception ex) {
+            System.err.println("Fie not exists or content error!");
+        }
+    }
+}
