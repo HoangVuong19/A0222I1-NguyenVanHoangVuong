b823166275f4444172aae7b7273727bb752b6949
HoangVuong19 <vuonghoang0208@gmail.com>
2022-10-17 17:40:39 +0700

    nop bai ngay 17-10

--- module4/_17_spring_boot/thuc_hanh/student_management/src/main/java/com/codegym/student_management/model/Student.java
+++ module4/_17_spring_boot/thuc_hanh/student_management/src/main/java/com/codegym/student_management/model/Student.java
@@ -3,8 +3,10 @@ package com.codegym.student_management.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Getter
 @Setter
@@ -18,6 +20,8 @@ public class Student {
     private Long id;
     private String studentName;
     private String age;
+    @Column(columnDefinition = "DATE")
+    private Date dateOfBirth;
 
     @ManyToOne
     @JoinColumn(name = "class_room_id")
