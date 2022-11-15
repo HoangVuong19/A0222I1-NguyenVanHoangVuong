7e3266427b065b48fa551cd07d6194a4adeb9c85
HoangVuong19 <vuonghoang0208@gmail.com>
2022-11-15 21:53:40 +0700

    nop bai ngay 15-11

--- module4/_12_jquery_ajax/bai_tap/demo/demo/src/main/java/com/example/demo/service/ICategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.models.Category;
-
-import java.util.List;
-
-public interface ICategoryService {
-    Iterable<Category> findAll();
-}
--- module4/_12_jquery_ajax/bai_tap/demo/phone/src/main/java/com/codegym/phone/repository/IPhoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.phone.repository;
-
-import com.codegym.phone.model.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPhoneRepository extends JpaRepository<Phone, Long> {
-}
